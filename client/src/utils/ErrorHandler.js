166e8935c5641ab96d4fba3c1ae9cb7bb3f3490f
uttamthegoat <19516uttam@gmail.com>
2024-10-02 19:05:57 +0530

    [Updated]:ErrorHandler in frontend

--- client/src/utils/ErrorHandler.js
+++ client/src/utils/ErrorHandler.js
@@ -1,14 +1,20 @@
 /* eslint-disable no-unused-vars */
-import {toast} from 'react-toastify';
+import { toast } from "react-toastify";
+import { Navigate } from "react-router-dom";
 
-  const ErrroHandler = (func) => {
-    return (req, res, next) => {
-      func(req, res, next).catch((error) => {
-        const message = error.response.data.message,
-          type = "error";
-          toast(message);
-      });
-    };
+const ErrroHandler = (func) => {
+  const navigate = Navigate();
+  return (req, res, next) => {
+    func(req, res, next).catch((error) => {
+      const message = error.response.data.message,
+        type = "error";
+      toast(message);
+      if (error.response.data.status === "logout") {
+        localStorage.removeItem("authenticate");
+        navigate("/auth");  // might have to change this
+      }
+    });
   };
+};
 
-  export default ErrroHandler;
+export default ErrroHandler;
