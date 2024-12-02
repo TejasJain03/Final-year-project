2a5225e96ef5454db6a3d2e503a0087eee7262e2
unknown <tejaskjain2003@gmail.com>
2024-12-02 14:54:43 +0530

    Docker compose File added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Enable hosting on 0.0.0.0 for access outside the container
+    port: 5173, // Ensure the port matches Docker's exposed port
+    watch: {
+      usePolling: true, // Use polling to handle file watching inside Docker
+    },
+  },
+});
+
